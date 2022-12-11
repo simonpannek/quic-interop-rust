3753c8a41a409c06e6952da2cdb8f0090879f525
Simon Pannek <simon.pannek@tum.de>
2022-12-11 22:00:17 +0100

    Minor changes

--- quic-project/implementation/build.sh
+++ quic-project/implementation/build.sh
@@ -8,4 +8,4 @@ git rev-parse HEAD > VERSION
 
 # Add all necessary files to the zip
 # Do NOT remove the three scripts
-zip -r artifact.zip VERSION setup-env.sh run-client.sh run-server.sh 
+zip -r artifact.zip VERSION setup-env.sh run-client.sh run-server.sh target/release/client target/release/server
