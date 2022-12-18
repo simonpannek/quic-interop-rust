bc854c26824977d4ddb006824308abe444b8ce2b
Simon Pannek <simon.pannek@tum.de>
2022-12-18 12:53:16 +0100

    Use zip binary

--- quic-project/implementation/build.sh
+++ quic-project/implementation/build.sh
@@ -12,4 +12,4 @@ git rev-parse HEAD > VERSION
 
 # Add all necessary files to the zip
 # Do NOT remove the three scripts
-zip -r artifact.zip VERSION setup-env.sh run-client.sh run-server.sh target/release/client target/release/server
+./zip -r artifact.zip VERSION setup-env.sh run-client.sh run-server.sh target/release/client target/release/server
